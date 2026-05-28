694e50f8f3579977334756cea54fd92053c2dac0
anushka1008-meena <anushkameena1008@gmail.com>
2026-05-28 21:58:46 +0530

    Added Assignment1

---
+++ Assignment1/Q1.py
@@ -0,0 +1,11 @@
+# 1) Practice print statement: - with multiple object - with seperator - with "ends" 
+
+
+print("Hello", "Python", 2026)   # multiple objects
+
+
+print("28", "05", "2026", sep='/')   # here we use ('/') seperator
+
+
+print("Good", end=' ')  # use of end = it keeps output on same line with space
+print("Morning")
---
+++ Assignment1/Q2.py
@@ -0,0 +1,5 @@
+# 2) Using input ask user to enter his/her name and output user name with a greeting message
+
+name = input('enter your name:')
+
+print(f'Good evening..! {name}')
