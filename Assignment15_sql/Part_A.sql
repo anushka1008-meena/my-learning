64a438bf82e073722a8bf2e847427de07e61c2b3
anushka1008-meena <anushkameena1008@gmail.com>
2026-06-07 23:34:39 +0530

    Added SQL data_bank assignment

---
+++ Assignment15_sql/Part_A.sql
@@ -0,0 +1,105 @@
+use data_bank;
+
+/* CREATE TABLE regions (
+  region_id INTEGER,
+  region_name VARCHAR(9)
+);
+
+INSERT INTO regions
+  (region_id, region_name)
+VALUES
+  ('1', 'Australia'),
+  ('2', 'America'),
+  ('3', 'Africa'),
+  ('4', 'Asia'),
+  ('5', 'Europe');   */
+
+-- we have converted the data of customer_transaction & customer_nodes into CSV file & imported it
+
+
+SELECT COUNT(*) FROM regions;
+
+SELECT COUNT(*) FROM customer_nodes;
+
+SELECT COUNT(*) FROM customer_transactions;
+
+
+-- PART A  =  Customer Nodes Exploration
+
+
+-- Q1. How many unique nodes are there on the Data Bank system?
+
+select count(distinct node_id) as unique_nodes
+from customer_nodes;
+
+
+
+
+--------------------------------------------------------------------------------
+
+-- Q2. What is the number of nodes per region?
+
+select
+r.region_name, count(distinct c.node_id) as total_nodes
+from customer_nodes c
+inner join regions r
+on c.region_id = r.region_id
+group by r.region_name;
+
+
+
+
+---------------------------------------------------------------------
+-- Q3. How many customers are allocated to each region?
+
+select
+r.region_name, count(distinct c.customer_id) as total_customers 
+from customer_nodes c
+inner join regions r
+on c.region_id = r.region_id
+group by r.region_name;
+
+
+
+
+
+------------------------------------------------------------------------
+
+-- Q4. How many days on average are customers reallocated to a different node?
+
+select
+avg(datediff(day, start_date, end_date)) as avg_reallocation_days  -- datediff = gives date difference 
+from customer_nodes
+where end_date != '9999-12-31';            -- -- removing customers still on same node
+
+
+
+
+
+---------------------------------------------------------------------------------------------------------
+
+-- q5. What is the median, 80th and 95th percentile for this same reallocation days metric for each region
+
+select distinct
+r.region_name,
+
+    percentile_cont(0.5)
+    within group (order by datediff(day, c.start_date, c.end_date))
+    over(partition by r.region_name) as median,
+
+    percentile_cont(0.8)
+    within group (order by datediff(day, c.start_date, c.end_date))
+    over(partition by r.region_name) as percentile_80,
+
+    percentile_cont(0.95)
+    within group (order by datediff(day, c.start_date, c.end_date))
+    over(partition by r.region_name) as percentile_95
+
+from customer_nodes c
+inner join regions r
+on c.region_id = r.region_id
+
+where end_date != '9999-12-31';
+
+
+                                                                                                                 ś
---
+++ Assignment15_sql/Part_B.sql
@@ -0,0 +1,70 @@
+use data_bank;
+
+-- PART B  =  Customer Transactions 
+
+-- Q1. What is the unique count and total amount for each transaction type?
+
+select
+txn_type,
+count(*) as transaction_count,
+sum(txn_amount) as total_amount
+from customer_transactions
+group by txn_type;
+
+
+
+
+
+-------------------------------------------------------------------------------
+
+-- q2. What is the average total historical deposit counts and amounts for all customers?
+
+select
+avg(deposit_count) as avg_deposit_count,
+avg(total_amount) as avg_deposit_amount
+
+from (
+       select
+       customer_id,
+       count(*) as deposit_count,
+       sum(txn_amount) as total_amount
+       from customer_transactions
+       where txn_type = 'deposit'
+       group by customer_id
+     ) as deposits;
+
+
+
+
+
+----------------------------------------------------------------------
+
+-- Q3. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month? 
+
+select
+month_no, count(customer_id) as customer_count
+
+from (
+
+    select
+    customer_id,
+    month(txn_date) as month_no,
+
+    sum(case when txn_type = 'deposit' then 1 else 0 end) as deposit_count,
+
+    sum(case when txn_type = 'purchase' then 1 else 0 end) as purchase_count,
+
+    sum(case when txn_type = 'withdrawal' then 1 else 0 end) as withdrawal_count
+
+    from customer_transactions
+
+    group by
+    customer_id,
+    month(txn_date)
+
+) as monthly_data
+
+where deposit_count > 1  and (purchase_count >= 1  or  withdrawal_count >= 1)
+
+group by month_no
+order by month_no;
